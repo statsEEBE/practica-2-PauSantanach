cf305d5a3d33a62aecd32e12e30dfc95e36362fd
pau.santanach <pau.santanach@auleseebe.upc.edu>
2024-10-03 11:32:18 +0200

    Problema 3 codi

--- Problema1.R
+++ Problema1.R
@@ -1,2 +1,15 @@
 #Codigo para problema 1
 
+#la resposta a la pregunta 1 és molt senzilla, és la R^2 = 0.1923 que la trobem a les dades.
+
+#la resposta a la preguna 2 és senzilla, la trobem a partir de l'R^2 (que està directament a l'enunciat de l'exercici --> 0.1923 i) fem la seva arrel i trobem directament el coeficient de correlació
+
+#per la pregunta 3 també només cal llegir la taula, serà la pendent que és 0.06576 ja que ens demanen quina variació esperem del valor del mercat per a 1 unitat dels actius és a dir sabem que la pendent = deltay/deltax, com que deltax ens diuen que és 1 llavors deltay(que és el que ens demanen) és igual a la pendent
+
+
+m <- 0.0658
+b <- 11.798
+x <- 647.1
+
+m*x+b
+#això que acabem de fer ha set la resposta a la pregunta 4
